6a31da64adfa3a46901410c8dc87ed24c07c05c5
Josh Newman <josh.newman@with.me>
2016-01-02 13:05:17 -0700

    initial code commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = roles => {
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    return function* (next) {
+        if (!this.state || !this.state.user) {
+            return yield next;
+        }
+
+        const userRoles = this.state.user.roles;
+        const foundRoles = roles.filter(r => userRoles.some(ur => ur === r));
+
+        if (!foundRoles.length) {
+            this.throw(403);
+        }
+
+        yield next;
+    };
+};
